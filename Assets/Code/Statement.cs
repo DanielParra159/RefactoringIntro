fa812dafad05b628075d7ce3aa0fc61ff037c186
Daniel Parra <dani.parra.90@gmail.com>
2021-01-04 14:32:59 +0100

    Extract function - AmountFor

--- Assets/Code/Statement.cs
+++ Assets/Code/Statement.cs
@@ -19,30 +19,8 @@ namespace Code
             foreach (var perf in invoice.Performances)
             {
                 var play = plays[perf.PlayId];
-                var thisAmount = 0;
 
-                switch (play.Type)
-                {
-                    case "tragedy":
-                        thisAmount = 40000;
-                        if (perf.Audience > 30)
-                        {
-                            thisAmount += 1000 * (perf.Audience - 30);
-                        }
-
-                        break;
-                    case "comedy":
-                        thisAmount = 30000;
-                        if (perf.Audience > 20)
-                        {
-                            thisAmount += 10000 + 500 * (perf.Audience - 20);
-                        }
-
-                        thisAmount += 300 * perf.Audience;
-                        break;
-                    default:
-                        throw new Exception($"Unknown type: {play.Type}");
-                }
+                var thisAmount = AmountFor(perf, play);
 
                 // add volume credits
                 volumeCredits += Mathf.Max(perf.Audience - 30, 0);
@@ -57,6 +35,36 @@ namespace Code
             result += $"Amount owed is {format(totalAmount / 100f)}\n";
             result += $"You earned {volumeCredits} credits\n";
             return result;
+        }
+
+        private int AmountFor(Performance perf, Play play)
+        {
+            var thisAmount = 0;
+
+            switch (play.Type)
+            {
+                case "tragedy":
+                    thisAmount = 40000;
+                    if (perf.Audience > 30)
+                    {
+                        thisAmount += 1000 * (perf.Audience - 30);
+                    }
+
+                    break;
+                case "comedy":
+                    thisAmount = 30000;
+                    if (perf.Audience > 20)
+                    {
+                        thisAmount += 10000 + 500 * (perf.Audience - 20);
+                    }
+
+                    thisAmount += 300 * perf.Audience;
+                    break;
+                default:
+                    throw new Exception($"Unknown type: {play.Type}");
+            }
+
+            return thisAmount;
         }
     }
 }
