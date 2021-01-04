e117b6c337c4196ac82e29a105bf6e19fa6b6dde
Daniel Parra <dani.parra.90@gmail.com>
2021-01-04 14:45:32 +0100

    Rename thisAmount

--- Assets/Code/Statement.cs
+++ Assets/Code/Statement.cs
@@ -39,32 +39,32 @@ namespace Code
 
         private int AmountFor(Performance perf, Play play)
         {
-            var thisAmount = 0;
+            var result = 0;
 
             switch (play.Type)
             {
                 case "tragedy":
-                    thisAmount = 40000;
+                    result = 40000;
                     if (perf.Audience > 30)
                     {
-                        thisAmount += 1000 * (perf.Audience - 30);
+                        result += 1000 * (perf.Audience - 30);
                     }
 
                     break;
                 case "comedy":
-                    thisAmount = 30000;
+                    result = 30000;
                     if (perf.Audience > 20)
                     {
-                        thisAmount += 10000 + 500 * (perf.Audience - 20);
+                        result += 10000 + 500 * (perf.Audience - 20);
                     }
 
-                    thisAmount += 300 * perf.Audience;
+                    result += 300 * perf.Audience;
                     break;
                 default:
                     throw new Exception($"Unknown type: {play.Type}");
             }
 
-            return thisAmount;
+            return result;
         }
     }
 }
