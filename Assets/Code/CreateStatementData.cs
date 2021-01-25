c835ae6312b51a0b560eddbe91368c2e668d8289
Daniel Parra <dani.parra.90@gmail.com>
2021-01-25 15:04:20 +0100

    Extract PerformanceCalculatorFactory

--- Assets/Code/CreateStatementData.cs
+++ Assets/Code/CreateStatementData.cs
@@ -1,19 +1,18 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
 namespace Code
 {
     public class CreateStatementData
     {
         private Dictionary<string, Play> _plays;
+
         public StatementData Generate(Invoice invoice, Dictionary<string, Play> plays)
         {
             _plays = plays;
             return GenerateStatementData(invoice);
         }
-        
+
         private StatementData GenerateStatementData(Invoice invoice)
         {
             var performancesData = EnrichPerformances(invoice);
@@ -30,7 +29,8 @@ namespace Code
             for (var i = 0; i < invoice.Performances.Length; i++)
             {
                 var performance = invoice.Performances[i];
-                var performanceCalculator = CreatePerformanceCalculator(performance, PlayFor(performance));
+                var performanceCalculator = new PerformanceCalculatorFactory()
+                   .Create(performance, PlayFor(performance));
                 performancesData[i] = new PerformanceData(performance.PlayId,
                                                           performance.Audience,
                                                           performanceCalculator.Play,
@@ -41,18 +41,6 @@ namespace Code
             return performancesData;
         }
 
-        private PerformanceCalculator CreatePerformanceCalculator(Performance performance, Play play)
-        {
-            switch (play.Type)
-            {
-                case "tragedy":
-                    return new TragedyCalculator(performance, play);
-                case "comedy":
-                    return new ComedyCalculator(performance, play);
-                default:
-                    throw new Exception($"Unknown type: {play.Type}");
-            }
-        }
 
         private int TotalAmount(PerformanceData[] performance)
         {
@@ -69,7 +57,5 @@ namespace Code
             var play = _plays[perf.PlayId];
             return play;
         }
-
-        
     }
 }
